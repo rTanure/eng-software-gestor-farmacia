bfe5f8d8d3166bb76aa4498d7e0f474126f35dd4
Richardy Tanure <richardy.tanure@hotmail.com>
2025-01-26 17:18:15 -0300

    feat: Configura beans do Spring

--- api/src/main/java/com/example/farmacia/infraestrutura/controllers/ExampleController.java
+++ api/src/main/java/com/example/farmacia/infraestrutura/controllers/ExampleController.java
@@ -3,12 +3,15 @@ package com.example.farmacia.infraestrutura.controllers;
 import com.example.farmacia.dominio.dtos.BuscaExampleDTO;
 import com.example.farmacia.dominio.models.Example;
 import com.example.farmacia.infraestrutura.services.IExampleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/example")
 public class ExampleController {
+
+    @Autowired
     IExampleService exampleService;
 
     @GetMapping()
--- api/src/main/java/com/example/farmacia/infraestrutura/repositories/ExampleRepositorio.java
+++ api/src/main/java/com/example/farmacia/infraestrutura/repositories/ExampleRepositorio.java
@@ -2,6 +2,8 @@ package com.example.farmacia.infraestrutura.repositories;
 
 import com.example.farmacia.infraestrutura.entidades.ExampleEntidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExampleRepositorio extends JpaRepository<ExampleEntidade, Long> {
 }
