d660c3cff7ab55b16a5dfe6b9bdbbe4e9ed3950f
Richardy Tanure <richardy.tanure@hotmail.com>
2025-03-03 16:00:08 -0300

    feat: Adiciona links para login e registro na home

--- client/src/ui/pages/home/Home.tsx
+++ client/src/ui/pages/home/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <h1>Sistema de gerenciamento de farmacias</h1>
-      <button>Login</button>
-      <button>Registrar</button>
+      <Link to="/auth/login">Login</Link>
+      <Link to="/auth/register">Registrar</Link>
     </div>
   );
 }
