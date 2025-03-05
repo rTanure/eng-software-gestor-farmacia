a3eeee98851f3faaf1cf22d33da80da93e41c89c
KemellySt <kemellysilva942@gmail.com>
2025-03-05 19:47:47 -0300

    pagina home criada

---
+++ client/FarmaciaApplicationFront/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import '../index.css';
+
+return (
+  <Box>
+    <Box></Box>
+  </Box>
+);
