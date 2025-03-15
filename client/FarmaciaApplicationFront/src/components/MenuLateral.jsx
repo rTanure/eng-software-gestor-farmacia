59e705ba3585218bbc5e9b14da631846e4e5002c
KemellySt <kemellysilva942@gmail.com>
2025-03-14 21:10:30 -0300

    Update

--- client/FarmaciaApplicationFront/src/components/MenuLateral.jsx
+++ client/FarmaciaApplicationFront/src/components/MenuLateral.jsx
@@ -13,7 +13,7 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import InventoryIcon from '@mui/icons-material/Inventory';
 
 const drawerWidth = 240;
 
@@ -73,10 +73,10 @@ export default function PermanentDrawerLeft() {
             mt: 3,
           }}
         >
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {['Estoque', 'Fornecedores', 'Vendas', 'Receitas', 'Clientes'].map((text, index) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemIcon>{index % 2 === 0 ? <InventoryIcon /> : <InboxIcon />}</ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
