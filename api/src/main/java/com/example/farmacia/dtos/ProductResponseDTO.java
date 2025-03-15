49b234e74ee3d91e13e2928c5adcb67c1bfd3383
LucasLeme04 <lucaslemeamaral@gmail.com>
2025-03-15 18:38:48 -0300

    Merge pull request #22 from rTanure/KemellySt-patch-13
    Criação da DTO ProductResponseDTO com o objetivo de realizar a consulta de um produto.

---
+++ api/src/main/java/com/example/farmacia/dtos/ProductResponseDTO.java
@@ -0,0 +1,36 @@
+package com.example.farmacia.dtos;
+
+import com.example.farmacia.entidades.Product;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+public class ProductResponseDTO {
+    private UUID id;
+    private String name;
+    private String code;
+    private String batch;
+    private LocalDate expirationDate;
+    private int receivedAmount;
+    private double purchasePrice;
+    private UUID supplierId;
+
+    public static ProductResponseDTO fromProduct(Product product) {
+        return ProductResponseDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .code(product.getCode())
+                .batch(product.getBatch())
+                .expirationDate(product.getExpirationDate())
+                .receivedAmount(product.getReceivedAmount())
+                .purchasePrice(product.getPurchasePrice())
+                .supplierId(product.getSupplierId())
+                .build();
+    }
+}
