c2c42432e271b0e6586a698880c54375f851de04
Pedro Alves <pedropaula2325p@gmail.com>
2025-03-18 09:57:03 -0300

    feat: adiciona UUID na classe Client

--- api/src/main/java/com/example/farmacia/entidades/Client.java
+++ api/src/main/java/com/example/farmacia/entidades/Client.java
@@ -1,15 +1,24 @@
 package com.example.farmacia.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
+@Entity
 @Getter
 @Setter
-@AllArgsConstructor
-@Builder
 public class Client {
+    @Id
+    @GeneratedValue(generator = "UUID", strategy = GenerationType.AUTO)
+    private UUID id;
+
     private String name;
     private String cpf;
     private String email;
