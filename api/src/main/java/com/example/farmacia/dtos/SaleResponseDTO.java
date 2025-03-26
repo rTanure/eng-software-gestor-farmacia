ba4b1976337c3f51d44f26a969b583a928e19b75
LucasLeme04 <lucaslemeamaral@gmail.com>
2025-03-26 15:28:28 -0300

    Merge pull request #22 from rTanure/KemellySt-patch-13
    Implementação completa do SaleRequestDTO e do SaleResponseDTO.

--- api/src/main/java/com/example/farmacia/dtos/SaleResponseDTO.java
+++ api/src/main/java/com/example/farmacia/dtos/SaleResponseDTO.java
@@ -1,11 +1,38 @@
 package com.example.farmacia.dtos;
 
+import com.example.farmacia.entidades.Sale;
+import com.example.farmacia.enums.EnumPaymenthMethod;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Getter
 @Setter
 @Builder
 public class SaleResponseDTO {
+    private UUID id;
+    private String nameClient; // nome do cliente
+    private String nameProduct; // nome do produto
+    private String codeProduct; // código de barras
+    private EnumPaymenthMethod paymenthMethod; // metodo de pagamento
+    private LocalDate paymenthDate;
+    private int amount; // quantidade vendida
+    private double salePrice; // preço de venda
+
+    // Converte a entidade para DTO
+    public static SaleResponseDTO fromSale(Sale sale) {
+        return SaleResponseDTO.builder()
+                .id(sale.getId())
+                .nameClient(sale.getNameClient())
+                .nameProduct(sale.getNameProduct())
+                .codeProduct(sale.getCodeProduct())
+                .paymenthMethod(sale.getPaymenthMethod())
+                .paymenthDate(sale.getPaymenthDate())
+                .amount(sale.getAmount())
+                .salePrice(sale.getSalePrice())
+                .build();
+    }
 }
