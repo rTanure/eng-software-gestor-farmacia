bbf645c3d1ca5cd8dce2f8baf72f1fc9748c9782
LucasLeme04 <lucaslemeamaral@gmail.com>
2025-03-26 16:55:08 -0300

    Merge pull request #22 from rTanure/KemellySt-patch-13
    Implementação do método para salvar compra no SaleController passando os dados pelo SaleRequestDTO.

--- api/src/main/java/com/example/farmacia/controllers/SaleController.java
+++ api/src/main/java/com/example/farmacia/controllers/SaleController.java
@@ -1,7 +1,14 @@
 package com.example.farmacia.controllers;
 
+import com.example.farmacia.dtos.SaleRequestDTO;
+import com.example.farmacia.dtos.SaleResponseDTO;
+import com.example.farmacia.entidades.Sale;
 import com.example.farmacia.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SaleController {
     @Autowired
     private SaleService saleService;
+
+    // Metodo para salvar uma venda
+    @PostMapping("/saveSale")
+    public ResponseEntity<SaleResponseDTO> saveSale(@RequestBody SaleRequestDTO saleRequestDTO) {
+        SaleResponseDTO saved = saleService.saveSale(saleRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
 }
