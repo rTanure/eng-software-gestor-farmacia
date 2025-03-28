3487893b7b2cfddbabf5fea3b58df5ef7d775fa8
Richardy Tanure <richardy.tanure@hotmail.com>
2025-03-28 11:50:58 -0300

    refactor: remove exemplos

--- api/src/main/java/com/example/farmacia/repositories/ExampleRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.farmacia.repositories;
-
-import com.example.farmacia.entidades.Example;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExampleRepositorio extends JpaRepository<Example, Long> {
-}
