80293bf549269beecc08a0e1724da1c400ae8e66
Richardy Tanure <richardy.tanure@hotmail.com>
2025-03-28 11:54:56 -0300

    fix: Voltei o Spring Security para o que era antes. Cabuloso demais mexer aqui

--- api/src/main/java/com/example/farmacia/infra/security/SecurityFilter.java
+++ api/src/main/java/com/example/farmacia/infra/security/SecurityFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,10 +15,16 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
     private final TokenService tokenService;
+    @Autowired
     private final UserRepositorio userRepositorio;
+
+    public SecurityFilter(TokenService tokenService, UserRepositorio userRepositorio) {
+        this.tokenService = tokenService;
+        this.userRepositorio = userRepositorio;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
