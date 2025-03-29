3f4c8adfe675d28d8daf82e1a5fc8a4ed600d9fc
KemellySt <kemellysilva942@gmail.com>
2025-03-29 01:08:39 -0300

    Update-Rotas

--- client/src/routes/RouterApp.tsx
+++ client/src/routes/RouterApp.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import { useAuthentication } from "../hooks/useAuthentication";
-import Estoque from "../ui/pages/app/Estoque";
+import Estoque from "../ui/pages/app/modules/Estoque";
 import { modules } from "../ui/pages/app/modules";
 import AppContainer from "../ui/pages/app/AppContainer";
 
 export default function RouterApp() {
-  const {logout} = useAuthentication()
+  const { logout } = useAuthentication();
   return (
     <div>
       <nav>
@@ -14,13 +14,15 @@ export default function RouterApp() {
       </nav>
       <Routes>
         <Route path="/" element={<Navigate to={"estoque"} />} />
-        {
-          modules.map(module => (
-            <Route key={module.path} path={module.path} element={<AppContainer children={<module.component />} />} />
-          ))
-        }
+        {modules.map((module) => (
+          <Route
+            key={module.path}
+            path={module.path}
+            element={<AppContainer children={<module.component />} />}
+          />
+        ))}
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </div>
-  )
+  );
 }
--- client/src/routes/RouterAuth.tsx
+++ client/src/routes/RouterAuth.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../ui/pages/auth/login";
 import Register from "../ui/pages/auth/register";
+import Estoque from "../ui/pages/app/modules/Estoque";
+import EstoqueAdd from "../ui/pages/app/modules/EstoqueAdd";
 export const RouterAuth = () => {
   return (
     <Routes>
@@ -9,6 +11,7 @@ export const RouterAuth = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="*" element={<div>Not Found</div>} />
+      <Route path="/estoqueadd" element={<EstoqueAdd />} />
     </Routes>
   );
 };
