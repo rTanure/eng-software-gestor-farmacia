6e2270ea73eea7c14fd4536682e09ce224da67d5
LucasLeme04 <lucaslemeamaral@gmail.com>
2025-03-30 20:58:16 -0300

    Merge pull request #22 from rTanure/KemellySt-patch-13
    Criação dos atributos tanto no SupplierRequestDTO quanto no SupplierResponseDTO, além da implementação do método fromSupplier.

--- api/src/main/java/com/example/farmacia/dtos/request/SupplierRequestDTO.java
+++ api/src/main/java/com/example/farmacia/dtos/request/SupplierRequestDTO.java
@@ -6,4 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SupplierRequestDTO {
+    private String companyName; // nome da empresa
+    private String cnpj; // CNPJ
+    private String address; // endereço
+    private String phone; // telefone
+    private String email; // e-mail
+    private String category; // categoria
 }
--- api/src/main/java/com/example/farmacia/dtos/response/SupplierResponseDTO.java
+++ api/src/main/java/com/example/farmacia/dtos/response/SupplierResponseDTO.java
@@ -1,11 +1,34 @@
 package com.example.farmacia.dtos.response;
 
+import com.example.farmacia.entidades.Supplier;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @Builder
 public class SupplierResponseDTO {
+    private UUID id;
+    private String companyName; // nome da empresa
+    private String cnpj; // CNPJ
+    private String address; // endereço
+    private String phone; // telefone
+    private String email; // e-mail
+    private String category; // categoria
+
+    // Converte a entidade para DTO
+    public static SupplierResponseDTO fromSupplier(Supplier supplier) {
+        return SupplierResponseDTO.builder()
+                .id(supplier.getId())
+                .companyName(supplier.getCompanyName())
+                .cnpj(supplier.getCnpj())
+                .address(supplier.getAddress())
+                .phone(supplier.getPhone())
+                .email(supplier.getEmail())
+                .category(supplier.getCategory())
+                .build();
+    }
 }
