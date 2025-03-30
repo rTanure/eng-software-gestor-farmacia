9dec9718eef817979c64f22ecee31f194e4bd349
LucasLeme04 <lucaslemeamaral@gmail.com>
2025-03-30 20:59:24 -0300

    Merge pull request #22 from rTanure/KemellySt-patch-13
    Implementação dos métodos de criação e busca por nome da companhia no SupplierController.

--- api/src/main/java/com/example/farmacia/controllers/SupplierController.java
+++ api/src/main/java/com/example/farmacia/controllers/SupplierController.java
@@ -1,9 +1,13 @@
 package com.example.farmacia.controllers;
 
+import com.example.farmacia.dtos.request.SupplierRequestDTO;
+import com.example.farmacia.dtos.response.SupplierResponseDTO;
 import com.example.farmacia.services.SupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/supplier")
@@ -12,4 +16,21 @@ public class SupplierController {
     @Autowired
     private SupplierService supplierService;
 
+    // Metodo para criar um fornecedor
+    @PostMapping("/createSupplier")
+    public ResponseEntity<SupplierResponseDTO> createSupplier(@RequestBody SupplierRequestDTO supplierRequestDTO) {
+        SupplierResponseDTO supplierResponseDTO = supplierService.createSupplier(supplierRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(supplierResponseDTO);
+    }
+
+    // Metodo para buscar um fornecedor por nome da companhia
+    @GetMapping("/searchByCompanyName")
+    public ResponseEntity<Page<SupplierResponseDTO>> searchSupplierByCompanyName(@RequestParam String companyName,
+                                                                           @RequestParam(defaultValue = "0") int page,
+                                                                           @RequestParam(defaultValue = "10") int size,
+                                                                           @RequestParam(defaultValue = "name") String sortBy,
+                                                                           @RequestParam(defaultValue = "asc") String order) {
+        Page<SupplierResponseDTO> suppliers = supplierService.findSupplierByCompanyName(companyName, page, size, sortBy, order);
+        return ResponseEntity.ok(suppliers);
+    }
 }
