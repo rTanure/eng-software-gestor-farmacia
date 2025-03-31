40c9150d8463fd796f61fcdf27b03c728bbaabf3
LucasLeme04 <lucaslemeamaral@gmail.com>
2025-03-31 13:58:42 -0300

    Merge pull request #22 from rTanure/KemellySt-patch-13
    Mudanças feitas no metodo de busca no SupplierService implementando List e Filter.

--- api/src/main/java/com/example/farmacia/services/SupplierService.java
+++ api/src/main/java/com/example/farmacia/services/SupplierService.java
@@ -1,19 +1,17 @@
 package com.example.farmacia.services;
 
 import com.example.farmacia.dtos.request.SupplierCreatRequestDTO;
-import com.example.farmacia.dtos.response.SupplierResponseDTO;
+import com.example.farmacia.dtos.request.SupplierFilterRequestDTO;
 import com.example.farmacia.entidades.Supplier;
 import com.example.farmacia.repositories.SupplierRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -34,11 +32,22 @@ public class SupplierService {
         supplierRepository.save(entity);
     }
 
-    // Metodo para buscar um fornecedor por nome da companhia
-    public Page<SupplierResponseDTO> findSupplierByCompanyName(String companyName, int page, int size, String sortBy, String order) {
-        Sort.Direction direction = Sort.Direction.fromString(order);
-        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortBy));
-        Page<Supplier> suppliers = supplierRepository.findByCompanyNameIgnoreCase(companyName, pageable);
-        return suppliers.map(SupplierResponseDTO::fromSupplier);
+    // Metodo para buscar um fornecedor
+    public List<Supplier> findByFilter(SupplierFilterRequestDTO supplierFilterRequestDTO) {
+        Supplier s = new Supplier();
+
+        // Copia as propriedades do DTO para a entidade
+        BeanUtils.copyProperties(s, supplierFilterRequestDTO);
+
+        // Cria um exemplo para a busca
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnoreCase()
+                .withIgnoreNullValues()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example<Supplier> example = Example.of(s, matcher);
+
+        // Retorna todos os fornecedores que correspondem ao exemplo
+        return supplierRepository.findAll(example);
     }
 }
