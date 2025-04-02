b5e7471da591e4c1264fea0ad8799cd15207f665
LucasLeme04 <lucaslemeamaral@gmail.com>
2025-04-02 07:52:59 -0300

    Merge pull request #22 from rTanure/KemellySt-patch-13
    Criação do UpdateRequest para edição de estoque pelo id.

---
+++ api/src/main/java/com/example/farmacia/dtos/request/ProductUpdateRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.farmacia.dtos.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ProductUpdateRequestDTO {
+    private UUID id;
+}
